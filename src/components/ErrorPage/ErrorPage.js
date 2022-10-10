492b37deb29b21ce5c45377725ad71b102443a05
Amit Sarker <amitsarker255@gmail.com>
2022-10-10 23:35:16 +0600

    Error page added

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import "./ErrorPage.css";
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>This is errror page</h1>
+    <div className="text-center text-5xl error text-red-600 font-extrabold">
+      <h1>404</h1>
+      <p className="font-sans text-blue-700">Looks like you lost.</p>
+      <p>
+        <small className="text-blue-500 text-3xl">
+          The page you are looking for not available
+        </small>
+      </p>
     </div>
   );
 };
